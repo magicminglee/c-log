600034657d57a658e2f6f1a5ab2d99bc6f640518
magicminglee <magicminglee@gmail.com>
2015-12-25 14:43:51 +0800

    rename log.cpp

--- log.cpp
+++ log.cpp
@@ -21,7 +21,7 @@ bool log_init(LogLevel l, const char* p_modulename, const char* p_logdir)
 			fprintf(stderr, "create folder failed\n");
 	}
 	char _location_str[_LOG_PATH_LEN];
-	snprintf(_location_str, _LOG_PATH_LEN, "%s/%s.access", p_logdir, p_modulename);	
+	snprintf(_location_str, _LOG_PATH_LEN, "%s/%s.access", p_logdir, p_modulename);
 	INFO_W.loginit(l, _location_str);
 	snprintf(_location_str, _LOG_PATH_LEN, "%s/%s.error", p_logdir, p_modulename);
 	//warning级别以上日志去WARN_W  去向由宏决定的 请见macro_define.h
@@ -48,7 +48,7 @@ const char* Log_Writer::logLevelToString(LogLevel l) {
 				return "UNKNOWN";
         }
 }
-	
+
 bool Log_Writer::checklevel(LogLevel l)
 {
 	if(l >= m_system_level)
@@ -61,24 +61,24 @@ bool Log_Writer::loginit(LogLevel l, const  char *filelocation, bool append, boo
 {
 	MACRO_RET(NULL != fp, false);
     m_system_level = l;
-    m_isappend = append; 
-    m_issync = issync; 
+    m_isappend = append;
+    m_issync = issync;
 	if(strlen(filelocation) >= (sizeof(m_filelocation) -1))
 	{
-		fprintf(stderr, "the path of log file is too long:%d limit:%d\n", strlen(filelocation), sizeof(m_filelocation) -1);
+		fprintf(stderr, "the path of log file is too long:%lu limit:%lu\n", strlen(filelocation), sizeof(m_filelocation) -1);
 		exit(0);
 	}
 	//本地存储filelocation  以防止在栈上的非法调用调用
 	strncpy(m_filelocation, filelocation, sizeof(m_filelocation));
 	m_filelocation[sizeof(m_filelocation) -1] = '\0';
-	
+
 	if('\0' == m_filelocation[0])
 	{
 		fp = stdout;
 		fprintf(stderr, "now all the running-information are going to put to stderr\n");
 		return true;
 	}
-	
+
 	fp = fopen(m_filelocation, append ? "a":"w");
 	if(fp == NULL)
 	{
@@ -95,7 +95,7 @@ int Log_Writer::premakestr(char* m_buffer, LogLevel l)
 {
     time_t now;
 	now = time(&now);;
-	struct tm vtm; 
+	struct tm vtm;
     localtime_r(&now, &vtm);
     return snprintf(m_buffer, _LOG_BUFFSIZE, "%s: %02d-%02d %02d:%02d:%02d ", logLevelToString(l),
             vtm.tm_mon + 1, vtm.tm_mday, vtm.tm_hour, vtm.tm_min, vtm.tm_sec);
@@ -106,16 +106,16 @@ bool Log_Writer::log(LogLevel l, char* logformat,...)
 	MACRO_RET(!checklevel(l), false);
 	int _size;
 	int prestrlen = 0;
-	
+
 	char * star = m_buffer;
 	prestrlen = premakestr(star, l);
 	star += prestrlen;
-	
+
 	va_list args;
 	va_start(args, logformat);
 	_size = vsnprintf(star, _LOG_BUFFSIZE - prestrlen, logformat, args);
 	va_end(args);
-	
+
 	if(NULL == fp)
 		fprintf(stderr, "%s", m_buffer);
 	else
@@ -126,7 +126,7 @@ bool Log_Writer::log(LogLevel l, char* logformat,...)
 bool Log_Writer::_write(char *_pbuffer, int len)
 {
 	if(0 != access(m_filelocation, W_OK))
-	{	
+	{
 		pthread_mutex_lock(&m_mutex);
 		//锁内校验 access 看是否在等待锁过程中被其他线程loginit了  避免多线程多次close 和init
 		if(0 != access(m_filelocation, W_OK))
@@ -143,7 +143,7 @@ bool Log_Writer::_write(char *_pbuffer, int len)
           	fflush(fp);
 		*_pbuffer='\0';
     }
-    else 
+    else
 	{
         int x = errno;
 	    fprintf(stderr, "Failed to write to logfile. errno:%s    message:%s", strerror(x), _pbuffer);
@@ -154,7 +154,7 @@ bool Log_Writer::_write(char *_pbuffer, int len)
 
 LogLevel Log_Writer::get_level()
 {
-	return m_system_level; 
+	return m_system_level;
 }
 
 bool Log_Writer::logclose()
